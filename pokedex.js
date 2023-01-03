48aa712446040660c5bceb467495e799ac5a124b
Ricardo-Vargas-Gonzalez <ceinnova05251@innovaccion.mx>
2023-01-03 14:22:15 -0600

    update

--- pokedex.js
+++ pokedex.js
@@ -1,9 +1,9 @@
-const fetchPokemon = () => {
+const fetchPokemon = async () => {
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
     pokeName = pokeName.toLowerCase();
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
-    fetch(url).then((res) => {
+    let data = await fetch(url).then((res) => {
         if (res.status != "200") {
             console.log(res);
             pokeImage("./pokemon-sad.gif")
@@ -11,14 +11,13 @@ const fetchPokemon = () => {
         else {
             return res.json();
         }
-    }).then((data) => {
-        if (data) {
-            console.log(data);
-            let pokeImg = data.sprites.front_default;
-            pokeImage(pokeImg);
-            console.log(pokeImg);
-        }
-    });
+    })
+    if (data) {
+        console.log(data);
+        let pokeImg = data.sprites.front_default;
+        pokeImage(pokeImg);
+        console.log(pokeImg);
+    }
 }
 
 const pokeImage = (url) => {
